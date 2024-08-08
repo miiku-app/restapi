b61567ccc4a94fdd2c18e2520889de9d6a67f772
Miku <miiku.app@gmail.com>
2024-08-08 14:33:47 -0300

    commit inicial

---
+++ src/main/java/com/example/restapi/controller/PersonController.java
@@ -0,0 +1,37 @@
+package com.example.restapi.controller;
+
+import com.example.restapi.model.Person;
+import com.example.restapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/persons")
+public class PersonController {
+
+    @Autowired
+    private PersonService personService;
+
+    @GetMapping
+    public List<Person> getAllPersons() {
+        return personService.getAllPersons();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Person> getPersonById(@PathVariable Long id) {
+        return personService.getPersonById(id);
+    }
+
+    @PostMapping
+    public Person createPerson(@RequestBody Person person) {
+        return personService.savePerson(person);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePerson(@PathVariable Long id) {
+        personService.deletePerson(id);
+    }
+}
